80a4758b8de6566f31327438906f73ffe7d55d94
zhaohongwan <zhaohongwan@cmail.carleton.ca>
2020-04-05 11:47:09 -0400

    Add files via upload

--- pixel.py
+++ pixel.py
@@ -4,31 +4,26 @@ from edge import gaussian_filter
 from edge import non_max_suppression
 from edge import convolution
 from edge import sobel
+from image import drawSame
+import sys
+from matplotlib import pyplot as plt
 
-img = cv2.imread("img.jpg", cv2.IMREAD_GRAYSCALE)
-gray = cv2.GaussianBlur(img, (5,5), 0)
+#img = cv2.imread("img.jpg", cv2.IMREAD_GRAYSCALE)
+front, left, top = drawSame()
+gray = cv2.GaussianBlur(front, (5,5), 0)
 g, d = sobel(gray)
 nms = non_max_suppression(g, d)
 
-#document to save pixel for checking
-def doc(image):
-    with open("123.txt", 'w') as f:
-        for x in range(image.shape[0]):
-            for y in range(image.shape[1]):
-                f.write(str(image[x,y]) + " ")
-            f.write("\n")
-    f.close()
+final = np.empty(nms.shape, dtype = np.float32)
+for i in range(nms.shape[0]):
+	for j in range(nms.shape[1]):
+		if nms[i,j] > 0:
+			final[i,j] = 1
+		else:
+			final[i,j] = 0
+plt.imshow(final)
+plt.show()
 
-def main():
-    final = np.empty(nms.shape, dtype = np.float32)
-    for i in range(nms.shape[0]):
-        for j in range(nms.shape[1]):
-            if nms[i,j] > 0:
-                final[i,j] = nms[i,j]
 
-    doc(final)
-    cv2.imwrite("final.jpg", final)
-    cv2.imshow("final", final)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-main()
+
+
