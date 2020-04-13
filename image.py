4056bb2a9c022537e5b7099d467cac2e09ed8f33
Roy Xu <roy.xu@carleton.ca>
2020-04-13 04:07:21 -0400

    finished mapping

--- image.py
+++ image.py
@@ -1,25 +1,32 @@
 import numpy as np
 import sys
 from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from scipy.interpolate import griddata
+from mapping import mapping
 # np.set_printoptions(threshold=sys.maxsize)
 
 def drawSame():
     front, left, top = np.zeros((200,200)), np.zeros((200,200)), np.zeros((200,200))
-    left[25:175, 50:150] = (left + 1)[25:175, 50:150]
-    top[20:180, 20:180] = (top + 1)[20:180, 20:180]
-    front[40:160, 60:140] = (front + 1)[40:160, 60:140]
+    left[50:150, 50:150] = (left + 1)[50:150, 50:150]
+    top[50:150, 25:175] = (top + 1)[50:150, 25:175]
+    front[50:150, 25:175] = (front + 1)[50:150, 25:175]
 
     return front, left, top
 
 
 front, left, top = drawSame()
 
-# print(front[25:50, 40:50])
+data = mapping(front, left, top, 100, 100, 100, 100)
+x, y, z = zip(*data)
+# z = list(map(float, z))
+# grid_x, grid_y = np.mgrid[min(x):max(x):10j, min(y):max(y):10j]
+# grid_z = griddata((x, y), z, (grid_x, grid_y))
 
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.scatter(x, y, z)
 # plt.imshow(front)
-# plt.show()
+plt.show()
 # plt.imshow(left)
 # plt.show()
-# plt.imshow(top)
-# plt.show()
-
--- mapping.py
+++ mapping.py
@@ -2,13 +2,104 @@ import numpy as np
 
 ##@param verticalCenter: center berween widest points on vertical axis
 ##@param horizontalCenter: center berween widest points on horizontal axis
-def mapping(front, left, top, verticalCenter, horizontalCenter):
-    result, l, r = [], 0, 0 # l is the left point on the edge, same with r
+def mapping(front, left, top, verticalCenterL, verticalCenterF, horizontalCenter, horizontalCenterF):
+    result = [] 
+    result.extend(mapFrontBack(front, left, top, verticalCenterL, horizontalCenter))
+    result.extend(mapTopBottom(front, left, top, verticalCenterL, horizontalCenterF))
+    result.extend(mapLeftRight(front, left, top, verticalCenterL, verticalCenterF, horizontalCenter))
 
-    for z in range(0, front.shape[0]):
-        if np.sum(front[z]) == 0:
+    return result
+
+def mapFrontBack(front, left, top, verticalCenter, horizontalCenter):
+    collect = []
+    for row in range(front.shape[0]):
+        if np.sum(front[row]) == 0:
             continue
-        for x in range(0, front.shape[1]):
-            if x == 0:
-                continue
+        leftMost = nonzerofromLeft(left, row)
+        rightMost = nonzerofromRight(left, row)
+        for col in range(front.shape[1]):
+            if front[row][col] != 0:
+                frontMost = nonzerofromFront(top, col)
+                if abs(rightMost - verticalCenter) < abs(frontMost - horizontalCenter):
+                    collect.append((col, rightMost, front.shape[0] - row))
+                else:
+                    collect.append((col, frontMost, front.shape[0] - row))
+                
+                backMost = nonzerofromBack(top, col)
+                if abs(leftMost - verticalCenter) < abs(backMost - horizontalCenter):
+                    collect.append((col, leftMost, front.shape[0] - row))
+                else:
+                    collect.append((col, backMost, front.shape[0] - row))
+    return collect
+
+def mapLeftRight(front, left, top, verticalCenterL, verticalCenterF, horizontalCenterT):
+    collect = []
+    for row in range(left.shape[0]):
+        if np.sum(left[row]) == 0:
+            continue
+        leftMost = nonzerofromLeft(front, row)
+        rightMost = nonzerofromRight(front, row)
+        for col in range(left.shape[1]):
+            if left[row][col] != 0:
+                frontMost = nonzerofromLeft(top, col)
+                if abs(leftMost - verticalCenterF) < abs(frontMost - verticalCenterF):
+                    collect.append((leftMost, col, left.shape[0] - row))
+                else:
+                    collect.append((frontMost, frontMost, left.shape[0] - row))
+                
+                backMost = nonzerofromRight(top, col)
+                if abs(rightMost - verticalCenterF) < abs(backMost - verticalCenterF):
+                    collect.append((rightMost, col, left.shape[0] - row))
+                else:
+                    collect.append((backMost, col, left.shape[0] - row))
+    return collect
+
+def mapTopBottom(front, left, top, verticalCenter, horizontalCenter):
+    collect = []
+    for row in range(top.shape[0]):
+        if np.sum(front[row]) == 0:
+            continue
+        downMost = nonzerofromFront(left, row)
+        upMost = nonzerofromBack(left, row)
+        for col in range(top.shape[1]):
+            if top[row][col] != 0:
+                frontMost = nonzerofromBack(front, col)
+                if abs(upMost - horizontalCenter) < abs(frontMost - horizontalCenter):
+                    collect.append((col, row, left.shape[0] - upMost))
+                else:
+                    collect.append((col, row, left.shape[0] - frontMost))
+                
+                backMost = nonzerofromFront(front, col)
+                if abs(downMost - horizontalCenter) < abs(backMost - horizontalCenter):
+                    collect.append((col, row, left.shape[0] - downMost))
+                else:
+                    collect.append((col, row, left.shape[0] - backMost))
+    return collect
+
+
+
+#used for left image
+def nonzerofromLeft(image, z):
+    for i in range(0, image.shape[1]):
+        if image[z][i] != 0:
+            return i
+
+def nonzerofromRight(image, z):
+    for i in range(image.shape[1]-1, -1, -1):
+        if image[z][i] != 0:
+            return i
+
+#used for top image
+def nonzerofromBack(image, x):
+    for i in range(0, image.shape[0]):
+        if image[i][x] != 0:
+            return i
+
+
+def nonzerofromFront(image, x):
+    for i in range(image.shape[0]-1, -1, -1):
+        # print(i, ', ', x)
+        if image[i][x] != 0:
+            return i
+
 
