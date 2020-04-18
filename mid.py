ca96f693113639c5ab3298b3e3baa5f6d084e2ef
zhaohongwan <zhaohongwan@cmail.carleton.ca>
2020-04-18 13:30:52 -0400

    Add files via upload

--- mid.py
+++ mid.py
@@ -6,7 +6,7 @@ def extract(i):
     #convert the image to grayscale
     g = cv2.cvtColor(i, cv2.COLOR_BGR2GRAY)
     #using threshold to get the edge of the object
-    _, thresh_gray = cv2.threshold(g, thresh=100, maxval=255, type=cv2.THRESH_BINARY_INV)
+    _, thresh_gray = cv2.threshold(g, thresh=1, maxval=255, type=cv2.THRESH_BINARY)
     image, contours,_ = cv2.findContours(thresh_gray, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
     matrix = (0,0,0,0)
@@ -25,10 +25,19 @@ def extract(i):
     cv2.imwrite('image2.png', roi)
     return roi
 
+#helper function to calculate the thresh value
+def thresh_help(img, lowThresholdRatio=0.05, highThresholdRatio=0.09):
+    highThreshold = img.max() * highThresholdRatio;
+    lowThreshold = highThreshold * lowThresholdRatio;
+    M, N = img.shape
+    res = np.zeros((M,N), dtype=np.int32)
+    weak = np.int32(25)
+    return weak
+
 #main function
 #def main():
-#    img = cv2.imread('image.png')
-#    gray = cv2.imread('image.png', cv2.IMREAD_GRAYSCALE)
+#    img = cv2.imread('Detection.jpg')
+    #gray = cv2.imread('Detection.png', cv2.IMREAD_GRAYSCALE)
 #    new = extract(img)
 #    cv2.imshow('new', new)
 #    cv2.imshow('old', img)
