0f6b6fdefb78ca3f0fdfc181ff21647f822c5a6a
zhaohongwan <zhaohongwan@cmail.carleton.ca>
2020-04-18 19:56:58 -0400

    Update mid.py

--- mid.py
+++ mid.py
@@ -4,10 +4,11 @@ import numpy as np
 #function to extract the object in the image
 def extract(i):
     #convert the image to grayscale
-    g = cv2.cvtColor(i, cv2.COLOR_BGR2GRAY)
+    #g = cv2.cvtColor(i, cv2.COLOR_BGR2GRAY)
+    i = i.astype(np.uint8)
     #using threshold to get the edge of the object
-    _, thresh_gray = cv2.threshold(g, thresh=1, maxval=255, type=cv2.THRESH_BINARY)
-    image, contours,_ = cv2.findContours(thresh_gray, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    _, thresh_gray = cv2.threshold(i, thresh=1, maxval=255, type=cv2.THRESH_BINARY)
+    _,contours,_ = cv2.findContours(thresh_gray, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
     matrix = (0,0,0,0)
     matrix_area = 0
@@ -30,17 +31,50 @@ def thresh_help(img, lowThresholdRatio=0.05, highThresholdRatio=0.09):
     highThreshold = img.max() * highThresholdRatio;
     lowThreshold = highThreshold * lowThresholdRatio;
     M, N = img.shape
-    res = np.zeros((M,N), dtype=np.int32)
+    res = np.zeros((M,N), dtype=np.uint8)
     weak = np.int32(25)
     return weak
 
-#main function
-#def main():
-#    img = cv2.imread('Detection.jpg')
-    #gray = cv2.imread('Detection.png', cv2.IMREAD_GRAYSCALE)
-#    new = extract(img)
-#    cv2.imshow('new', new)
-#    cv2.imshow('old', img)
-#    cv2.waitKey(0)
-#    cv2.destroyAllWindows()
-#main()
+#put img1 into the center of the img2
+def merge(img1, img2):
+    #the shape of the inserted image
+    h, w = img1.shape
+    print(h,w)
+    #thje shape of the background image
+    hh, ww = img2.shape
+    print(hh,ww)
+
+    # compute xoff and yoff for placement of upper left corner of resized image
+    yoff = round((hh-h)/2)
+    xoff = round((ww-w)/2)
+    print(yoff,xoff)
+
+    # use numpy indexing to place the resized image in the center of background image
+    result = img2.copy()
+    result[yoff:yoff+h, xoff:xoff+w] = img1
+
+    # view result
+    cv2.imshow('CENTERED', result)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    # save resulting centered image
+    cv2.imwrite('center.png', result)
+    return result
+
+#calculate the center of the iput image.
+def get_center(img):
+    x = img.shape[0]
+    y = img.shape[1]
+    x_c = 0
+    y_c = 0
+    if (x%2 == 0):
+        x_c = x/2-1
+    else:
+        x_c = x/2 + 0.5
+    if (y%2 == 0):
+        y_c = y/2-1
+    else:
+        y_c = y/2 + 0.5
+
+    print(x_c, y_c)
